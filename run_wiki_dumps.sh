bbb16c34e09ec0a063b0860190627ce4b8c1db15
Ipsedo <berriens@gmail.com>
2019-07-01 10:06:09 +0200

    minor change

--- run_wiki_dumps.sh
+++ run_wiki_dumps.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-
 LARGE_DIR=models/xlnet_cased_L-24_H-1024_A-16
 
 python run_classifier.py \
-  --use_tpu=False \
-  --use-gpu=False \
   --do_train=True \
   --do_eval=True \
   --eval_all_ckpt=True \
@@ -17,12 +14,12 @@ python run_classifier.py \
   --model_config_path=./${LARGE_DIR}/xlnet_config.json \
   --init_checkpoint=./${LARGE_DIR}/xlnet_model.ckpt \
   --max_seq_length=128 \
-  --train_batch_size=8 \
+  --train_batch_size=2 \
   --eval_batch_size=8 \
   --num_hosts=1 \
   --num_core_per_host=1 \
   --learning_rate=2e-5 \
-  --train_steps=40 \
-  --warmup_steps=10 \
-  --save_steps=10 \
-  --iterations=10
+  --train_steps=400 \
+  --warmup_steps=100 \
+  --save_steps=100 \
+  --iterations=100
