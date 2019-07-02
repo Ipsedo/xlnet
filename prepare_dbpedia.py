7a12524a1d19ef1ac198ac48bef71628b3827ea7
Ipsedo <berriens@gmail.com>
2019-07-02 14:32:48 +0200

    add preprocess dbpedia script

---
+++ prepare_dbpedia.py
@@ -0,0 +1,61 @@
+from tqdm import tqdm
+import re
+
+
+def filter_things(titles, labels):
+    new_titles = []
+    new_labels = []
+
+    for t, l in tqdm(zip(titles, labels)):
+        if l != "owl#Thing":
+            new_labels.append(l)
+            new_titles.append(t)
+    return new_titles, new_labels
+
+
+def create_types_dict(titles, labels):
+    return {t: l for t, l in zip(titles, labels)}
+
+
+def create_dataset(title_to_type, abstracts):
+    data_set = []
+
+    for line in tqdm(abstracts):
+        title = line.split(" ")[0].replace("<", "").replace(">", "").split("/")[-1]
+        try:
+            abst = re.search("\"(.+)\"", line).group(1)
+        except:
+            # Prevent empty abstracts
+            continue
+
+        if title in title_to_type:
+            data = (title_to_type[title], abst)
+            data_set.append(data)
+
+    return data_set
+
+
+def write_dataset(data_set, file_name):
+    out_file = open(file_name, "w")
+
+    for lbl, data in tqdm(data_set):
+        line = lbl + "|||" + data
+        out_file.write(line + "\n")
+
+
+if __name__ == "__main__":
+    instances_types = open("/home/samuel/Documents/Stage_SG/dbpedia/instance_types_en.ttl").readlines()[1:]
+    labels = [l.split(" ")[2].replace("<", "").replace(">", "").split("/")[-1] for l in instances_types]
+    titles = [l.split(" ")[0].replace("<", "").replace(">", "").split("/")[-1] for l in instances_types]
+
+    titles, labels = filter_things(titles, labels)
+
+    title_to_type = create_types_dict(titles, labels)
+
+    print(len(title_to_type))
+
+    long_abstracts = open("/home/samuel/Documents/Stage_SG/dbpedia/long_abstracts_en.ttl").readlines()[1:]
+
+    dataset = create_dataset(title_to_type, long_abstracts)
+
+    write_dataset(dataset, "dbpedia_pp.txt")
